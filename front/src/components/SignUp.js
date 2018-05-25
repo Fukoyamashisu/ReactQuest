dd1c64662a45980242b38aa19661b74af5461c42
Fukoyamashisu <anthony.troupenat@gmail.com>
2018-05-25 09:50:33 +0200

    add material ui

--- front/src/components/SignUp.js
+++ front/src/components/SignUp.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Grid,TextField,Button} from '@material-ui/core';
+import {AccountCircle,Send} from '@material-ui/icons';
+import homer from '../assets/images/Homer.png';
 
 
 
@@ -37,6 +40,9 @@ class SignUp extends Component {
 
     handleSubmit(e ,data){
       e.preventDefault();
+      console.log('====================================');
+      console.log(data);
+      console.log('====================================');
       this.setState({
           name:"",
           email:"",
@@ -65,37 +71,57 @@ class SignUp extends Component {
 
       const {data, flash, ...form }= this.state;
         return (
-            <div className="col-sm-8 offset-sm-2">
-              <div className="row justify-content-center">
-                <h1><pre>{JSON.stringify(form,null,2)}</pre></h1>
-              </div>
-              <div className="row justify-content-center">
-                <h3 className="alert-danger">{flash.length>0 && flash}</h3>
-              </div>
-              <form method="POST" action="/auth/signup" onSubmit={e => this.handleSubmit(e,form)} onChange={this.handleChange}>
-                <div className="form-group">
-                  <label htmlFor="inputName">Nom</label>
-                  <input type="text" id="inputName" className="form-control" placeholder="Votre Nom" name="name" required></input>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="inputEmail">Email</label>
-                  <input type="email" id="inputEmail" className="form-control" placeholder="wilder@wildcodeschool.fr" name="email" required/>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="inputPassword">Mot de Passe</label>
-                  <input type="password" id="inputPassword" className="form-control" placeholder="*******" name="password" required></input>
-                </div>
-                <div className="form-group">
-                  <label htmlFor="inputCheckPassword">Mot de Passe</label>
-                  <input type="password" id="inputCheckPassword" className="form-control" placeholder="*******" name="checkPassword" required></input>
-                </div>
-                <div className="row justify-content-center">
-                  <button className="btn btn-outline-dark">Envoyer</button>
-                </div>
-              </form>
+          <form onSubmit={e => this.handleSubmit(e,form)} onChange={this.handleChange}>
+          <Grid container justify="center" alignItems="center">
+            <Grid item sm={6}>
+              <Grid container direction="row" justify="center" alignItems="center">
+                <Grid item>
+                  <img src={homer} alt="homer" />
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item sm={6}>
+              <Grid item sm={12 }>
+                <Grid container direction="row" justify="center">
+                  <h3 className="alert-danger">{flash.length > 0 && flash}</h3>
+                </Grid>
+              </Grid>
+              <Grid item sm={12}>
+                  <Grid container direction="row" justify="center" alignItems="flex-end">
+                  <AccountCircle />
+                  <TextField label="Name" name="name" style={{width:"80%"}}/>
+                </Grid>
+              </Grid>
+              <Grid item sm={12}>
+                  <Grid container direction="row" justify="center" alignItems="flex-end">
+                  <AccountCircle />
+                  <TextField label="Email"  name="email" style={{ width: "80%" }} />
+                </Grid>
+              </Grid>
+              <Grid item sm={12}>
+                  <Grid container direction="row" justify="center" alignItems="flex-end">
+                    <AccountCircle />
+                    <TextField label="Password" name="password" style={{ width: "80%" }} />
+                </Grid>
+              </Grid>
+                <Grid item sm={12}>
+                  <Grid container direction="row" justify="center" alignItems="flex-end">
+                  <AccountCircle />
+                  <TextField label="Confirm password" name="checkPassword" style={{ width: "80%" }}/>
+                </Grid>
+              </Grid>
+              <Grid item sm={12} style={{margin:"20px 0"}}>
+                <Grid container direction="row" justify="center">
+                <Button type="submit" variant="raised" color="primary">
+                  Send
+                 <Send style={{marginLeft:"10px"}}/>
+                </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+          </form>
 
-
-            </div>
         );
     }
 }
